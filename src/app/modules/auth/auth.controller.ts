671a1f2cd08ef5b24768f1105dae7a7e45794e4d
sakincse21 <saleheen.sakin@gmail.com>
2025-12-12 22:46:09 +0600

    final fix in the backend

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,37 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { catchAsync } from "../../utils/catchAsync";
 import { Request, Response, NextFunction } from "express";
 import { AuthServices } from "./auth.service";
-import httpStatus from 'http-status';
+import httpStatus from "http-status";
 import { sendResponse } from "../../utils/sendResponse";
 import { setAuthCookie } from "../../utils/setAuthCookies";
+import { envVars } from "../../config/env";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const login = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const {user, accessToken} = await AuthServices.login(req.body)
+const login = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { user, accessToken } = await AuthServices.login(req.body);
 
-    setAuthCookie(res,accessToken)
-
-    sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.CREATED,
-        message: "User Created Successfully",
-        data: {...user, accessToken}
-    })
-})
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
-    setAuthCookie(res,"")
+    setAuthCookie(res, accessToken);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", envVars.FRONTEND_URL); // or specific origin
 
     sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.OK,
-        message: "User Logged Out Successfully",
-        data: null
-    })
-})
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User Created Successfully",
+      data: { ...user, accessToken },
+    });
+  }
+);
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    setAuthCookie(res, "");
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Logged Out Successfully",
+      data: null,
+    });
+  }
+);
 
 export const AuthControllers = {
-    login,
-    logout
-}
+  login,
+  logout,
+};
